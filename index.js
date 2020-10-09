94d8e98746e59ba540875db993f6d8e4d7920077
jonelle-noelani <jonelle.noelani@gmail.com>
2020-10-09 03:44:08 -0700

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,26 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString){
+  let splitTime = timeString.split(':').join('');
+  console.log(typeof splitTime); //string
+  let parseTime = parseInt(splitTime);
+  console.log(typeof parseTime); //number
+  // let splitTime = timeString.split(":");
+  // console.log("hit the greet")
+  if (parseTime < 1200){
+    greeting = "Good Morning";
+  } else if (parseTime < 1700){
+    greeting = "Good Afternoon";
+  } else {
+    greeting = "Good Evening";
+  }
+  return greeting
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(greeting){
+  document.getElementById("greeting").innerText = greeting
+  // updateGreet = greeting;
+  // console.log(updateGreet);
+}
+// let content = document.getElementById("greeting").innerText
